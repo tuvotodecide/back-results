6c8b728ed09e952ed342de6112f42e014945df66
Huascar <huascarm@gmail.com>
2025-09-23 12:18:33 -0400

    main yaml right

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -12,19 +12,23 @@ import * as path from 'path';
         const uri = configService.get<string>('app.database.uri');
         const username = configService.get<string>('app.database.username');
         const password = configService.get<string>('app.database.password');
-        const caPath = path.resolve(__dirname, '../../global-bundle.pem');
+        const isLocal = process.env.NODE_ENV === 'development'; // O usa configService.get('app.env') == 'local'
+        const caPath = isLocal
+          ? undefined
+          : path.resolve(__dirname, '../../global-bundle.pem'); // Solo para no-local
+
         console.log('Database URI being used:', uri);
 
         return {
           uri,
-          authSource: 'admin',
+          authSource: username ? 'admin' : undefined, // Solo si hay username
           auth: username && password ? { username, password } : undefined,
           retryWrites: false,
           w: 'majority',
           maxPoolSize: 10,
-          tls: true,
-          tlsCAFile: caPath,
-          tlsAllowInvalidHostnames: true,
+          tls: !isLocal, // Desactiva TLS para local
+          tlsCAFile: caPath, // Solo si tls está activo
+          tlsAllowInvalidHostnames: !isLocal, // Solo si tls está activo
           directConnection: true,
           serverSelectionTimeoutMS: 30000,
         };
