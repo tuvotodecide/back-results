d1284b5f6fbc578ba5f32e8169cde8f29d8802d9
Juan Carlos Arevalo <jarevaloferrufino@gmail.com>
2026-01-16 21:06:52 -0400

    add: results filter tests

---
+++ test/utils/seeds/electionsSeed.ts
@@ -0,0 +1,41 @@
+import { Connection } from "mongoose";
+
+export async function seedElectionConfigWith(conn: Connection, electionKey: string) {
+  const election = electionConfigsSeed.get(electionKey);
+  if (!election) {
+    throw new Error(`No election config seed found for key: ${electionKey}`);
+  }
+
+  return await conn.collection('election_configs').insertOne(election);
+}
+
+const electionConfigsSeed = new Map<string, any>([
+  [
+    'activeElection',
+    {
+      "name": "Elecciones Generales 2025",
+      "votingStartDate": new Date(new Date().setDate(new Date().getDate() - 1)),
+      "votingEndDate": new Date(new Date().setDate(new Date().getDate() + 1)),
+      "resultsStartDate": new Date(new Date().setDate(new Date().getDate() + 2)),
+      "isActive": true,
+      "allowDataModification": true,
+      "timezone": "America/La_Paz",
+      "type": "presidential",
+      "round": 1,
+    }
+  ],
+  [
+    'pastElection',
+    {
+      "name": "Elecciones Generales 2024",
+      "votingStartDate": new Date(new Date().setDate(new Date().getDate() - 10)),
+      "votingEndDate": new Date(new Date().setDate(new Date().getDate() - 9)),
+      "resultsStartDate": new Date(new Date().setDate(new Date().getDate() - 8)),
+      "isActive": true,
+      "allowDataModification": true,
+      "timezone": "America/La_Paz",
+      "type": "presidential",
+      "round": 1,
+    }
+  ]
+]);
